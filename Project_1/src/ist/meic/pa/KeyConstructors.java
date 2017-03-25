d8ebcd8ea1d020c2be2cd6eef84d5c40de948d1a
Balatazar95 <tiago_mpb95@hotmail.com>
2017-03-25 13:24:38 +0000

    Final touch

--- Project_1/src/ist/meic/pa/KeyConstructors.java
+++ Project_1/src/ist/meic/pa/KeyConstructors.java
@@ -4,10 +4,12 @@ import javassist.ClassPool;
 import javassist.Loader;
 import javassist.Translator;
 
-public class KeyConstructors {
-
-	public static void main(String[] args) throws Throwable{
-		if(args.length == 0){
+public class KeyConstructors 
+{
+	public static void main(String[] args) throws Throwable
+	{
+		if(args.length == 0)
+		{
 			System.err.println("Profiler needs arguments");	
 			return;
 		}	
@@ -17,8 +19,6 @@ public class KeyConstructors {
 		classLoader.addTranslator(pool, translator);
 		String[] restArgs = new String[args.length - 1];
 		System.arraycopy(args, 1, restArgs, 0, restArgs.length);
-		classLoader.run(args[0], restArgs);
-		
+		classLoader.run(args[0], restArgs);		
 	}
-
 }
--- Project_1/src/ist/meic/pa/MyTranslator.java
+++ Project_1/src/ist/meic/pa/MyTranslator.java
@@ -2,7 +2,6 @@ package ist.meic.pa;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -13,12 +12,13 @@ import javassist.CtNewConstructor;
 import javassist.NotFoundException;
 import javassist.Translator;
 
-
 public class MyTranslator implements Translator
 {
 	public Map<String, String> map = new HashMap<String, String>();
 	public Map<String, String> convertTypes = new HashMap<String, String>();
 	
+	//In the start method of our translator we fill our map that contains all 
+	//the primitive types that we are going to use to cast our object variable
 	public void start(ClassPool pool) throws NotFoundException, CannotCompileException 
 	{
 		convertTypes.put("int", "Integer");
@@ -29,26 +29,24 @@ public class MyTranslator implements Translator
 		convertTypes.put("short", "Short");
 		convertTypes.put("byte", "Byte");
 		convertTypes.put("char", "Character");
-		convertTypes.put("java.lang.String", "String");
 	}
 	
+	//The onLoad method is used to catch the wanted class has it is being loaded.
 	public void onLoad(ClassPool pool, String className) throws NotFoundException, CannotCompileException 
 	{
-		CtClass ctClass = pool.get(className); 	//pool - the ClassPool that this translator should use.
-		//classname - the name of the class being loaded.
+		CtClass ctClass = pool.get(className); 	
 		try 
 		{
 			CheckKeywordArgs(ctClass);
-			
 		} 
 		catch (ClassNotFoundException e) 
 		{
-			//FIXME DEAL WITH EXCEPTION
 			e.printStackTrace();
 		}
 	}
 	
-
+	//This method checks for the KeywordArgs annotation. 
+	//Then it gets all the respective keywords associated with the passed class constructor.
 	public void CheckKeywordArgs(CtClass ctClass) throws ClassNotFoundException, CannotCompileException, NotFoundException 
 	{
 		for(CtConstructor ctConstructor: ctClass.getDeclaredConstructors())
@@ -61,6 +59,7 @@ public class MyTranslator implements Translator
 					SplitArgs(((KeywordArgs) annotation).value());
 					CheckKeywordArgs_aux(ctClass.getSuperclass());
 					Injection(ctClass, ctConstructor);
+					//This line of code adds a default constructor that was suppose to be created by java.
 					ctClass.addConstructor(CtNewConstructor.defaultConstructor(ctClass));
 					break;
 				}
@@ -68,9 +67,9 @@ public class MyTranslator implements Translator
 		}
 	}
 	
+	//This is an auxiliary method that is responsible for checking all the keywords in the super class constructor
 	public void CheckKeywordArgs_aux(CtClass ctClass) throws ClassNotFoundException, CannotCompileException, NotFoundException 
 	{
-
 		for(CtConstructor ctConstructor: ctClass.getDeclaredConstructors())
 		{	
 			//This loop will enable the programmers to have other annotations because it only cares with the KeywordArgs annotation
@@ -79,6 +78,7 @@ public class MyTranslator implements Translator
 				if(annotation instanceof KeywordArgs)
 				{
 					SplitArgs(((KeywordArgs) annotation).value());
+					//This is the stop condition for the recursive call of this method.
 					if(!ctClass.getSuperclass().getName().equals("java.lang.Object"))
 					{
 						CheckKeywordArgs_aux(ctClass.getSuperclass());
@@ -89,65 +89,79 @@ public class MyTranslator implements Translator
 		}
 	}
 	
+	//This method is responsible for the injection of the code in the caught class constructor.
 	public void Injection(CtClass ctClass, CtConstructor ctConstructor) throws CannotCompileException, NotFoundException
 	{
-		String body="{ boolean verifyVariable = false;"
-				+ "	   String[] check = new String[$1.length];";
+		String attributetype, attributeName;
+		//These two variables are for special cases we want to catch
+		//The verifyVariable its a boolean to check if there is a value for the attribute in the instantiation
+		//The check vector is used to verify if all the passed parameters in the instantiation are actual attributes of the class
+		String body="{ "
+				+ "		boolean verifyVariable = false;"
+				+ "	   	String[] check = new String[$1.length];";
 		
 		for(CtField ct : ctClass.getFields())
 		{
-			if(map.containsKey(ct.getName()))
+			attributeName = ct.getName();
+			if(map.containsKey(attributeName))
 			{
-				String type = ct.getType().getName();
-//				body = body + "System.out.println(\""+ct.getName()+"\");\n";
-//				System.out.println(ct.getName());	
-				body = body + "for(int i=0; i < $1.length; i++)\n"
-							+ "{\n"
-							+ "if($1[i].equals(\""+ ct.getName() +"\")){\n"
-									+ "check[i] = \"Here\";";
-
-				if(type.startsWith("java")){
-					String[] splitted = type.split("\\.");
-					body = body + ct.getName()  + "= (" + splitted[splitted.length-1] + ") $1[i+1];\n"
-								+ "           		verifyVariable = true;\n"
-								+ "           		i++;\n";
-					}
-				else{
-					body = body + ct.getName() + "= ((" + convertTypes.get(type)  + ") $1[i+1])." + type + "Value();\n"
-												+ " verifyVariable = true;\n"
-												+ " i++;\n"; 
-				}
-				body = body  + "check[i] = \"Here\";"
-						+ " }"
-						+ "} "
-						+ "if(!verifyVariable)\n"
-						+ "{\n";
+				attributetype = ct.getType().getName();
 				
-				if(!map.get(ct.getName()).equals(""))
+				body = body + "		for(int i=0; i < $1.length; i++)"
+							+ "		{"
+							+ "			if($1[i].equals(\""+ attributeName +"\"))"
+							+ "			{"
+							+ "				check[i] = \"Here\";";
+
+				//This condition checks the actual type we want to convert to
+				if(attributetype.startsWith("java"))
 				{
-					body = body + " " + ct.getName() + "=" + map.get(ct.getName()) + ";\n";
+					String[] splitted = attributetype.split("\\.");
+					body = body + attributeName  + "= (" + splitted[splitted.length-1] + ") $1[i+1];"
+								+ "           		verifyVariable = true;"
+								+ "           		i++;";
 				}
-				body = body + "}\n"
-						+ "else\n"
-						+ "{\n"
-						+ "     verifyVariable = false;\n"
-						+ "}\n";
+				else
+				{
+					body = body + attributeName + "= ((" + convertTypes.get(attributetype)  
+									+ ") $1[i+1])." + attributetype + "Value();"
+								+ " verifyVariable = true;"
+								+ " i++;"; 
+				}
+				
+				body = body + "			check[i] = \"Here\";"
+							+ "		}"
+							+ "} "
+							+ "if(!verifyVariable)"
+							+ "{";
+				
+				if(!map.get(attributeName).equals(""))
+				{
+					body = body + " " + attributeName + "=" + map.get(attributeName) + ";";
+				}
+				
+				body = body + "}"
+							+ "else"
+							+ "{"
+							+ "     verifyVariable = false;"
+							+ "}";
 			}
 		}
-		body = body + "for(int j = 0; j<$1.length; j++)"
-				+ "{"
-					+ "if(check[j] == null)"
-					+ "{"
-					+ "		throw new RuntimeException(\"Unrecognized keyword: \" + $1[j]);"
-					+ "}"
-					+ "}"
-				+ "}";
-	//	System.out.println(body);
+		
+		body = body + "		for(int j = 0; j<$1.length; j++)"
+					+ "		{"
+					+ "			if(check[j] == null)"
+					+ "			{"
+					+ "				throw new RuntimeException(\"Unrecognized keyword: \" + $1[j]);"
+					+ "			}"
+					+ "		}"
+					+ "}";
 		
 		ctConstructor.setBody(body);
 		map.clear();
 	}
 	
+	//This method is responsible for splitting the the keywords and their values and save them in a map.
 	public void SplitArgs(String args)
 	{
 		String[] paramValue; 
@@ -155,7 +169,10 @@ public class MyTranslator implements Translator
 		for(String param : parts)
 		{
 			paramValue = param.split("=");
-			if(!map.containsKey(paramValue[0]) || map.get(paramValue[0]).equals("")){
+			//This condition gives priority to the subclass keyword values and 
+			//overrides one if it is the predefined value of java
+			if(!map.containsKey(paramValue[0]) || map.get(paramValue[0]).equals(""))
+			{
 				if(paramValue.length == 1)
 				{
 					map.put(paramValue[0], "");
